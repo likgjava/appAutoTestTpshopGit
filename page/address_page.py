a8a8e629cc03f236ad0d8d0183a1a438eebccc27
likg <likg.java@163.com>
2018-07-08 22:54:08 +0800

    test

--- page/address_page.py
+++ page/address_page.py
@@ -13,9 +13,25 @@ class AddressPage(BasePage):
 
         # 新建地址
         self.add_address_btn = (By.ID, "com.tpshop.malls:id/add_address_btn")
+        # 编辑按钮
+        self.edit_address_btn = (By.XPATH, "//*[@text='{}']/../android.widget.ImageView")
+        # 删除按钮
+        self.del_address_btn = (By.XPATH, "//*[@text='删除']")
+        # 删除确定按钮
+        self.del_confirm_btn = (By.ID, "com.tpshop.malls:id/positiveButton")
 
     def find_add_address_btn(self):
         return self.find_element(self.add_address_btn)
+
+    def find_edit_address_btn(self, address):
+        location = (self.edit_address_btn[0], self.edit_address_btn[1].format(address))
+        return self.find_element(location)
+
+    def find_del_address_btn(self):
+        return self.find_element(self.del_address_btn)
+
+    def find_del_confirm_btn(self):
+        return self.find_element(self.del_confirm_btn)
 
 
 class AddressHandle(BaseHandle):
@@ -29,6 +45,15 @@ class AddressHandle(BaseHandle):
     def click_add_address_btn(self):
         self.address_page.find_add_address_btn().click()
 
+    def click_edit_address_btn(self, address):
+        self.address_page.find_edit_address_btn(address).click()
+
+    def click_del_address_btn(self):
+        self.address_page.find_del_address_btn().click()
+
+    def click_del_confirm_btn(self):
+        self.address_page.find_del_confirm_btn().click()
+
 
 class AddressProxy:
     """
@@ -40,4 +65,9 @@ class AddressProxy:
 
     def to_add_address_page(self):
         self.address_handle.click_add_address_btn()
+
+    def del_address(self, address):
+        self.address_handle.click_edit_address_btn(address)
+        self.address_handle.click_del_address_btn()
+        self.address_handle.click_del_confirm_btn()
 
--- script/test_address.py
+++ script/test_address.py
@@ -1,25 +1,18 @@
 import allure
 import pytest
-from selenium.common.exceptions import TimeoutException
 
 from common import utils
+from common.utils import YamlUtil
 from page.add_address_page import AddAddressProxy
 from page.address_page import AddressProxy
 from page.login_page import LoginProxy
 from page.main_page import MainProxy
 from page.mine_page import MineProxy
 from page.setting_page import SettingProxy
-from common.utils import DriverUtil
-from common.utils import YamlUtil
 
 
-def build_address_data():
-    # return [
-    #     ("13041092162", "tpshop12345678", "登录成功"),
-    #     ("13041092162", "tpshop1234567", "密码错误"),
-    #     ("1301234", "tpshop12345678", "账号不存在")
-    # ]
-    data = YamlUtil.get_data_by_key("address", "test_address")
+def build_add_address_data():
+    data = YamlUtil.get_data_by_key("address", "test_add_address")
     print(data)
     data_list = []
     for login in data.values():
@@ -30,6 +23,15 @@ def build_address_data():
     print("data_list=", data_list)
     return data_list
 
+
+def build_del_address_data():
+    data = YamlUtil.get_data_by_key("address", "test_del_address")
+    print(data)
+    data_list = []
+    for login in data.values():
+        data_list.append((login["address"], login["toast"]))
+    print("data_list=", data_list)
+    return data_list
 
 
 class TestAddress:
@@ -44,25 +46,27 @@ class TestAddress:
         cls.address_proxy = AddressProxy()
         cls.add_address_proxy = AddAddressProxy()
 
+        # 进入‘我的’页面
+        cls.main_proxy.to_mine_page()
+
+        # 进入‘收货地址’页面
+        cls.mine_proxy.to_address_page()
+
     # @classmethod
     # def teardown_class(cls):
     #     print('teardown_class')
     #     DriverUtil.quit_driver()
 
     def setup(self):
-        # 进入‘我的’页面
-        self.main_proxy.to_mine_page()
-
-        # 进入‘收货地址’页面
-        self.mine_proxy.to_address_page()
+        pass
 
     def teardown(self):
         print('teardown')
         # self.login_proxy.back_to_mine_page()
 
     @allure.step("新增收货地址")
-    @pytest.mark.parametrize("name,mobile,province,city,district,town,address,toast", build_address_data())
-    def test_address(self, name, mobile, province, city, district, town, address, toast):
+    @pytest.mark.parametrize("name,mobile,province,city,district,town,address,toast", build_add_address_data())
+    def test_add_address(self, name, mobile, province, city, district, town, address, toast):
         # print("test_address username={} password={} toast={}".format(username, password, toast))
         # allure.attach("用例参数：", "username={} password={} toast={}".format(username, password, toast))
 
@@ -71,4 +75,10 @@ class TestAddress:
 
         self.add_address_proxy.add_address(name, mobile, province, city, district, town, address)
 
+        assert utils.is_exist_toast(toast)
+
+    @allure.step("删除收货地址")
+    @pytest.mark.parametrize("address,toast", build_del_address_data())
+    def test_del_address(self, address, toast):
+        self.address_proxy.del_address(address)
         assert utils.is_exist_toast(toast)
